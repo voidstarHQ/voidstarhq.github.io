c02595c860ff380684ad7511d6d8d67aa34c60a9
Pierre Fenoll <pierrefenoll@gmail.com>
2023-03-27 17:08:31 +0200

    videos.sh Allow overriding current docker recipe
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- videos.sh
+++ videos.sh
@@ -5,11 +5,13 @@ FILE=${FILE:-}
 FILES_REPO=${FILES_REPO:-../files001.git}
 FILES_SLUG=${FILES_SLUG:-$(basename "$FILES_REPO" .git)}
 OUTPUT_DIR=${OUTPUT_DIR:-mp4}
+VOIDSTAR_REPO=${VOIDSTAR_REPO:-https://github.com/voidstarHQ/voidstar.git}
 
 echo FILE "$FILE"
 echo FILES_REPO "$FILES_REPO"
 echo FILES_SLUG "$FILES_SLUG"
 echo OUTPUT_DIR "$OUTPUT_DIR"
+echo VOIDSTAR_REPO "$VOIDSTAR_REPO"
 
 run() {
     local file=$1; shift
@@ -25,19 +27,27 @@ run() {
     url=https://github.com/voidstarHQ/"$FILES_SLUG"/raw/main/"$file"
     echo url "$url"
 
-    DOCKER_BUILDKIT=1 \
-      docker build \
-        --output="$tmp"/ \
-        --target=video-gcc \
-        --build-arg FILE="$url" \
-        https://github.com/voidstarHQ/voidstar.git \
-    || return 1
+    args=()
+    args+=(build)
+    args+=(--output="$tmp"/)
+    args+=(--target=video-gcc)
+    args+=(--build-arg=FILE="$url")
+    args+=(--build-arg=WxHxD=800x600x24)
+    if [[ "$VOIDSTAR_REPO" =~ ^https: ]]; then
+        args+=("$VOIDSTAR_REPO")
+    else
+        [[ -d "$VOIDSTAR_REPO" ]] || return 1
+        args+=(-f "$VOIDSTAR_REPO"/Dockerfile)
+        args+=("$VOIDSTAR_REPO")
+    fi
+    DOCKER_BUILDKIT=1 docker "${args[@]}" || return 1
 
     mv "$tmp"/video.mp4 "$out"
 }
 
 if [[ -n "$FILE" ]]; then
     run "$FILE"
+    exit
 fi
 
 url=https://api.github.com/repos/VoidstarHQ/"$FILES_SLUG"/git/trees/main
@@ -60,3 +70,5 @@ for file in $(curl -fsSL# "$url" \
     esac
 done
 exit "$errors"
+
+# https://adw0rd.github.io/instagrapi/usage-guide/media.html
